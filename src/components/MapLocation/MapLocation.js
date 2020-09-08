d7349631a258f4864602f7bd893c241eedc87858
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-08 09:55:39 +0100

    Add custom icon and city name and region in popup

--- src/components/MapLocation/MapLocation.js
+++ src/components/MapLocation/MapLocation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import LocationIcon from '../../components/LocationIcon/LocationIcon';
 
-function MapLocation({ coordinates }) {
+function MapLocation({ coordinates, city, region }) {
   const { lat, lng } = coordinates;
   return (
-    <Map center={[lat, lng]} zoom={13} styles={{ height: '350px' }}>
+    <Map center={[lat, lng]} zoom={13} style={{ height: '350px' }}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[lat, lng]}>
-        <Popup>Yo it's me</Popup>
+      <Marker position={[lat, lng]} icon={LocationIcon}>
+        <Popup>{`${city}, ${region}`}</Popup>
       </Marker>
     </Map>
   );
